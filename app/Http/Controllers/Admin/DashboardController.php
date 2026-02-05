8f40b2799c6f92dbd9fd589fa81ef40a39cf8032
bobymaulana <bobymaulanaa21@gmail.com>
2026-02-05 13:41:59 +0700

    fixing dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -19,7 +19,7 @@ class DashboardController extends Controller
     // 2. FILTER BERDASARKAN ROLE
     if ($user->role == 'staff') {
         // Staff cuma hitung data tokonya sendiri
-        $pendingOrders = $orders->where('toko_id', $user->toko_id)
+        $paidOrders = $orders->where('toko_id', $user->toko_id)
                                 ->where('status', 'sudah_bayar') // Staff fokus ke yang sudah bayar utk disiapkan
                                 ->count();
 
@@ -40,12 +40,12 @@ class DashboardController extends Controller
                                          ->get();
     } else {
         // ADMIN: Lihat Semuanya
-        $pendingOrders = \App\Models\Order::where('status', 'menunggu_bayar')->count();
+        $paidOrders = \App\Models\Order::where('status', 'sudah_bayar')->count();
         $totalRevenue = \App\Models\Order::whereIn('status', ['sudah_bayar', 'siap_diambil', 'selesai'])->sum('total_bayar');
         $totalResellers = \App\Models\User::where('role', 'reseller')->count();
         $recentOrders = \App\Models\Order::with(['user', 'toko'])->latest()->take(5)->get();
     }
 
-    return view('admin.dashboard', compact('pendingOrders', 'totalRevenue', 'totalResellers', 'recentOrders'));
+    return view('admin.dashboard', compact('paidOrders', 'totalRevenue', 'totalResellers', 'recentOrders'));
 }
 }
